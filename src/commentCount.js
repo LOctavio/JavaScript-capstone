6c69ce65bf47eed56d74a3c2c1d67729c7c26dd3
LOctavio <luis_ram1997@hotmail.com>
2021-08-13 14:00:04 -0500

    fix error with comment count

--- src/commentCount.js
+++ src/commentCount.js
@@ -2,6 +2,7 @@ const commentCount = (document, id) => {
   const container = document.querySelector(`#${id}_comments`);
   const numberOfItems = container.getElementsByTagName('p').length;
   const title = document.querySelector(`#${id}_count`);
+  title.textContent = 'Comments';
   title.textContent += `(${numberOfItems})`;
 };
 
