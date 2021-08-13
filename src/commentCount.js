380dafb37e7a1436700475303241df6f7dd95bc7
LOctavio <luis_ram1997@hotmail.com>
2021-08-13 09:50:35 -0500

    remove console log

--- src/commentCount.js
+++ src/commentCount.js
@@ -2,7 +2,6 @@ const commentCount = (document, id) => {
   const container = document.querySelector(`#${id}_comments`);
   const numberOfItems = container.childNodes.length;
   const title = document.querySelector(`#${id}_count`);
-  console.log(title.id);
   title.textContent += `(${numberOfItems})`;
 };
 
