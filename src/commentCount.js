212710811d60f5f4bddcab772e2b0df786a8c773
LOctavio <luis_ram1997@hotmail.com>
2021-08-13 10:26:37 -0500

    add test for comment count

--- src/commentCount.js
+++ src/commentCount.js
@@ -1,6 +1,6 @@
 const commentCount = (document, id) => {
   const container = document.querySelector(`#${id}_comments`);
-  const numberOfItems = container.childNodes.length;
+  const numberOfItems = container.getElementsByTagName('p').length;
   const title = document.querySelector(`#${id}_count`);
   title.textContent += `(${numberOfItems})`;
 };
